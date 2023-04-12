db00d0fb78d5ddb1babca9afb4ca917af8733540
Phillip Kim <pkim90@gatech.edu>
2023-04-12 15:34:55 -0400

    working error message

--- public/scripts/createUser.js
+++ public/scripts/createUser.js
@@ -1,5 +1,29 @@
 let createUserButton = document.getElementById("createUser");
-let personID = document.getElementById("personID");
+let perID = document.getElementById("perID");
 let password = document.getElementById("password");
 let confirmPassword = document.getElementById("confirmPassword");
-let message = document.getElementById("message");
+let errorMessage = document.getElementById("errors");
+
+
+function validate(event) {
+    errorMessage.hidden = true;
+    event.preventDefault();
+    
+    // Ensure fields are valid and passwords match
+    if (perID.value === "" || password.value === "" || confirmPassword.value === "") {
+        errorMessage.style.display = "block"
+        errorMessage.className = "alert alert-danger mt-4"
+        errorMessage.innerHTML = "Please fill out all fields.";
+        errorMessage.hidden = false;
+    } else if (password.value !== confirmPassword.value) {
+        errorMessage.style.display = "block"
+        errorMessage.className = "alert alert-danger mt-4"
+        errorMessage.innerHTML = "Passwords do not match.";
+        errorMessage.hidden = false;
+    } else {
+        createUserButton();
+    }
+}
+
+createUserButton.addEventListener("click", validate);
+
